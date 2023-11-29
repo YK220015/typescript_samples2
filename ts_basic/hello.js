e59b61381c349392b072c60d54e2a79fd37a3391
YK220015 <220015ab@yse-c.net>
2023-11-29 11:52:31 +0900

    a

---
+++ ts_basic/hello.js
@@ -0,0 +1,14 @@
+var message = "hello";
+console.log(message);
+var id = "1";
+var email = "test@a.x";
+var isActive = true;
+var none;
+none = "yes";
+none = 1;
+var memo;
+console.log(typeof memo);
+console.log(id, typeof id);
+console.log(email, typeof email);
+console.log(isActive, typeof isActive);
+console.log(none, typeof none);
